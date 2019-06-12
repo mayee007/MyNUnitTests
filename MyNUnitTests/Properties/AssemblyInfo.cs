0a7ff5351159dd137aa56bdeb044ce38081603e8
mayee007 <yelekeri@gmail.com>
2019-06-12 15:30:07 -0400

    Add project files.

---
+++ MyNUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyNUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyNUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d456360a-0235-468a-8b5b-d8a7c1f8a426")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MyNUnitTests/Tests/GUI/GoogleMainPageTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+namespace MyNUnitTests
+{
+    [Category("guiTests")]
+    [TestFixture]
+    public class GoogleMainPageTest
+    {
+        IWebDriver driver;
+        String url;
+        String title;
+
+        [SetUp]
+        public void setup()
+        {
+            // to run chrome headless 
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--headless"); 
+            driver = new ChromeDriver(options);
+
+            //driver = new ChromeDriver(); // this pops up windows
+            url = "http://google.com";
+            title = "Google";
+        }
+
+        [TestCase]
+        [Timeout(10000)] // 10 seconds 
+        public void openHomePageTest()
+        {
+            driver.Navigate().GoToUrl(url);
+            driver.Manage().Window.Maximize(); // maximizes window       
+            driver.Close(); //closes window                              
+        }
+
+        [TestCase] // method 1 - simple 
+        public void getTitleTest()
+        {
+            driver.Navigate().GoToUrl(url);
+            Assert.AreEqual(title, driver.Title);
+            driver.Close(); //closes window                              
+        }
+
+        // method 2 - more test cases 
+        [TestCase("Google", ExpectedResult = true)]
+        [TestCase("google", ExpectedResult = false)]
+        [TestCase("", ExpectedResult = false)]
+        [TestCase("Googleeeeee", ExpectedResult = false)]
+        [Timeout(20000)] //20 seconds
+        public Boolean getTitleTestMethod2(String title)
+        {
+            driver.Navigate().GoToUrl(url);
+            String actualTitle = driver.Title;
+            driver.Close(); //closes window  
+            
+            return actualTitle.Equals(title); 
+        }
+
+        [TearDown]
+        public void cleanup()
+        {
+            driver.Quit();
+        }
+    } // end of class
+}
